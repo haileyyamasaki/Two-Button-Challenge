bcaf864932742ef5266c387a9931647fd1e26e49
Hailey Yamasaki <hyamasaki@zagmail.gonzaga.edu>
2020-09-03 10:55:35 -0700

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Hailey Yamasaki on 9/3/20.
+//  Copyright © 2020 Hailey Yamasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
